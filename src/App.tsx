d6e58f620254d8ced4b250cd5863a4a2858284c1
Kangyoon Lee <rkddbs1031@gmail.com>
2026-01-23 16:33:10 +0900

    chor: React Router 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Home from './pages/Home';
+
+interface RouteConfig {
+  path: string;
+  element: React.ReactNode;
+}
+
+const routes: RouteConfig[] = [{ path: '/', element: <Home /> }];
+
 function App() {
-  return <div className="text-lg">APP</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {routes.map(({ path, element }) => (
+          <Route path={path} element={element} />
+        ))}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
