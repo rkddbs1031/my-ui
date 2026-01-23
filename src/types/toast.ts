5f7bff04625853e31c31d00d20d6e82ee4dc8f17
Kangyoon Lee <rkddbs1031@gmail.com>
2026-01-23 20:06:35 +0900

    feat: toast type 정의

--- src/types/toast.ts
+++ src/types/toast.ts
@@ -1,1 +1,22 @@
 export type ToastType = 'success' | 'error' | 'warning' | 'info';
+
+export interface Toast {
+  id: string;
+  title: string;
+  subtitle?: string;
+  type: ToastType;
+  duration: number;
+}
+
+export interface ToastOptions {
+  title: string;
+  subtitle: string;
+  duration: number;
+  type: ToastType;
+}
+
+export interface ToastState {
+  queue: Toast[];
+  currentQueue: Toast | null;
+  addToast: () => void;
+}
