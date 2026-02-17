a43e1500a9ea7805457e4082a7241bd0e95c9f45
Kangyoon Lee <rkddbs1031@gmail.com>
2026-02-17 13:52:40 +0900

    feat: eye, eyeClosed icon

---
+++ src/components/icons/EyeClosed.tsx
@@ -0,0 +1,23 @@
+interface Props {
+  className?: string;
+}
+
+export const EyeClosedIcon: React.FC<Props> = ({ className = '' }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+    >
+      <path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z" />
+      <circle cx="12" cy="12" r="3" />
+    </svg>
+  );
+};
