821870feaea867996c8de50f99456c48bfcabe0c
Kangyoon Lee <rkddbs1031@gmail.com>
2026-02-17 14:46:18 +0900

    fix: BaseInput props의 DOM 전달로 인한 콘솔 경고 해결

--- src/components/ui/input/BaseInput.tsx
+++ src/components/ui/input/BaseInput.tsx
@@ -1,4 +1,6 @@
-import type { InputProps } from '@/types/input';
+import type { InputHTMLAttributes } from 'react';
+
+import type { InputProps, NumberInput, PasswordInput } from '@/types/input';
 import { cn } from '@/utils';
 
 export function BaseInput({
@@ -7,8 +9,15 @@ export function BaseInput({
   className = '',
   ...rest
 }: InputProps) {
+  const { showToggle, hideSpinner, ...pureProps } = rest as Partial<
+    Pick<PasswordInput, 'showToggle'> & Pick<NumberInput, 'hideSpinner'>
+  > &
+    InputHTMLAttributes<HTMLInputElement>;
+  /* Partial : 혹시 있다면 꺼내겠다 선언. */
+
   return (
     <input
+      {...pureProps}
       disabled={isDisabled ?? true}
       className={cn(
         'w-full px-3 py-2 border rounded-lg focus:outline-none',
@@ -16,7 +25,6 @@ export function BaseInput({
         isDisabled && 'bg-gray-100 cursor-not-allowed opacity-60',
         className
       )}
-      {...rest}
     />
   );
 }
